84730062653ef6e4488294f3b483813c3b02a351
Mike Green <mike.is.green@gmail.com>
2009-04-29 11:06:36 -0400

    added conditional inclusion of stylesheets to index.php

--- index.php
+++ index.php
@@ -6,4 +6,40 @@
 	
 	$config = Spyc::YAMLLoad("config.yml");
 	$changelog = file_get_contents($config["changelog"]["file"]);
+	$html = Markdown($changelog);
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<?php
+		if (count($config["html"]["stylesheets"]["screen"]) > 0) {
+			foreach($config["html"]["stylesheets"]["screen"] as $stylesheet) {
+				echo '<link rel="stylesheet" type="text/css" media="screen, projection" href="' . $stylesheet . '" />';
+			} 
+		}
+		if (count($config["html"]["stylesheets"]["print"]) > 0) {
+			foreach($config["html"]["stylesheets"]["print"] as $stylesheet) {
+				echo '<link rel="stylesheet" type="text/css" media="print" href="' . $stylesheet . '" />';
+			}
+		}
+		if (count($config["html"]["stylesheets"]["ie"]) > 0) {
+			echo "<!--[if IE]>";
+			foreach($config["html"]["stylesheets"]["ie"] as $stylesheet) {
+				echo '<link rel="stylesheet" type="text/css" media="all" href="' . $stylesheet . '" />';
+			}
+			echo "<![endif]-->";
+		}
+	?>
+	
+	<title><?php echo $config["html"]["title"]; ?></title>
+	
+</head>
+
+<body>
+
+
+</body>
+</html>
