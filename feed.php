c629af98cb5cdc3f8fd5b20b150df7c366d945c9
Mike Green <mike.is.green@gmail.com>
2009-04-29 22:32:21 -0400

    halfway done with feed handling

--- feed.php
+++ feed.php
@@ -1,6 +1,12 @@
 <?php
 	require_once("includes/prolog.php");
 	
+	// Set content type to plain text for debugging:
+	header("Content-Type: text/plain");
+	
+	// Tell the browser we're sending an ATOM feed:
+	// header("Content-Type: application/atom+xml");
+	
 	function uuid($prefix = '') {
     	$chars = md5(uniqid(mt_rand(), true));
     	$uuid  = substr($chars,0,8) . '-';
@@ -11,31 +17,29 @@
     	return $prefix . $uuid;
   	}
 	
-	function getDateFromTagString($tag) {
-		$time = strtotime($tag->plaintext);
-		return $time;
-	}
-	
-	$date_format = "%Y-%m-%dT%h:%i:%sZ";
-	$date_headers = $dom->find("h2");
-	$dates = array_map("getDateFromTagString", $date_headers);
-	$reverse_dates = array_reverse($dates);
+	$date_format = "%Y-%m-%dT%H:%M:%SZ";
+	$last_updated = filemtime($config["changelog"]["file"]);
+	$entries = preg_split('/\-{4,}/', $changelog);
 ?>
 
-<?xml version="1.0" encoding="utf-8"?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
 	<title><?php print $config["feed"]["title"]; ?></title>
 	<?php if (isset($config["feed"]["subtitle"])): ?>
-		<subtitle><?php print $config["feed"]["subtitle"]; ?></subtitle>
+	<subtitle><?php print $config["feed"]["subtitle"]; ?></subtitle>
 	<?php endif; ?>
 	<link rel="alternate" href="<?php print $config['feed']['alternate']; ?>" />
 	<link rel="self" href="<?php print $_SERVER['REQUEST_URI']; ?>" />
-	<updated><?php print strftime($date_format, $reverse_dates[0]); ?></updated>
+	<updated><?php print strftime($date_format, $last_updated); ?></updated>
 	<author>
 		<name><?php print $config["feed"]["author_name"]; ?></name>
 		<email><?php print $config["feed"]["author_email"]; ?></email>
 	</author>
-	<id><?php print uuid("urn:uuid:"); ?></id>
+	<id><?php print $config["feed"]["id"]; ?></id>
 	
-	
+	<?php
+		foreach ($entries as $entry) {
+			
+		}
+	?>
 </feed>
